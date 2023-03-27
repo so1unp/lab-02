44ccdb4126f77b3a872ceb76800909dd43d67481
Francisco E. Paez <franpaez+github@gmail.com>
2023-03-27 15:37:21 -0300

    Actualiza script con scopy.c

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 copy_result=false
 
-if [ -f "bin/safecopy" ]; then
-    printf "Testing: bin/safecopy\n"
+if [ -f "bin/scopy" ]; then
+    printf "Testing: bin/scopy\n"
 
     printf "\tProbando que verifique argumentos: "
-    bin/safecopy > /dev/null 2>&1
+    bin/scopy > /dev/null 2>&1
     if [ $(echo $?) != 0 ]; then
         printf "Ok\n"
         exec1=true
@@ -16,8 +16,8 @@ if [ -f "bin/safecopy" ]; then
     fi
 
     printf "\tProbando copia de un archivo: "
-    bin/safecopy safecopy.c copia-de-safecopy.c > /dev/null 2>&1
-    diff="$(diff safecopy.c copia-de-safecopy.c 2>/dev/null)"
+    bin/scopy scopy.c copia-de-scopy.c > /dev/null 2>&1
+    diff="$(diff scopy.c copia-de-scopy.c 2>/dev/null)"
     if [ "$(echo $?)" = 0 ] && [ "$diff" = "" ] ; then
         printf "Ok\n"
         exec2=true
@@ -27,7 +27,7 @@ if [ -f "bin/safecopy" ]; then
     fi
 
     printf "\tVerificando permisos del archivo nuevo: "
-    perms=$(ls -l copia-de-safecopy.c 2>/dev/null | awk '{print $1}')
+    perms=$(ls -l copia-de-scopy.c 2>/dev/null | awk '{print $1}')
     if [ "$(echo $?)" = 0 ] && [ "$perms" = "-rw-r--r--" ]; then
         printf "Ok\n"
         exec3=true
@@ -35,12 +35,12 @@ if [ -f "bin/safecopy" ]; then
         printf "Error\n"
         exec3=false
     fi
-    rm -f copia-de-safecopy.c > /dev/null 2>&1
+    rm -f copia-de-scopy.c > /dev/null 2>&1
 
     printf "\tVerificando que no sobreescribe archivo destino: "
     echo "abc" > prueba1.txt
     echo "xyz" > prueba2.txt
-    bin/safecopy prueba1.txt prueba2.txt > /dev/null 2>&1
+    bin/scopy prueba1.txt prueba2.txt > /dev/null 2>&1
     if [ "$(diff prueba1.txt prueba2.txt)" = "" ]; then
         printf "Error\n"
         exec4=false
@@ -51,7 +51,7 @@ if [ -f "bin/safecopy" ]; then
     rm -f prueba1.txt prueba2.txt > /dev/null 2>&1
 
     printf "\tProbando copiar un archivo que no existe: "
-    bin/safecopy no-existes.c copia-de-no-existes.c > /dev/null 2>&1
+    bin/scopy no-existes.c copia-de-no-existes.c > /dev/null 2>&1
     if [ $(echo $?) != 0 ]; then
         printf "Ok\n"
         exec5=true
@@ -62,7 +62,7 @@ if [ -f "bin/safecopy" ]; then
     rm -f copia-de-no-existes.c > /dev/null 2>&1
 
     printf "\tProbando copiar un archivo a un directorio sin permisos: "
-    bin/safecopy safecopy.c /safecopy.c > /dev/null 2>&1
+    bin/scopy scopy.c /scopy.c > /dev/null 2>&1
     if [ $(echo $?) != 0 ]; then
         printf "Ok\n"
         exec6=true
@@ -75,7 +75,7 @@ if [ -f "bin/safecopy" ]; then
         copy_result=true
     fi
 else
-    printf "Error: safecopy.c no esta compilado.\n"
+    printf "Error: scopy.c no esta compilado.\n"
 fi
 
 if $copy_result ; then
