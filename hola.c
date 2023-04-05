592637b1c8fddc1ce2b2c5a761a9659c5754a5c2
Francisco E. Paez <franpaez+github@gmail.com>
2023-04-04 21:41:34 -0300

    Elimina argumentos de main() en hola.c

--- hola.c
+++ hola.c
@@ -5,10 +5,10 @@
 #include <stdio.h>      // printf()
 #include <stdlib.h>     // exit()
 
-int main(int argc, char *argv[])
+int main(void)
 {
     // Imprime un saludo en la salida estándar.
-    puts("¡Hola Mundo!\n");
+    puts("¡Hola Mundo!");
 
     // Termina la ejecución del proceso.
     exit(EXIT_SUCCESS);
